ac16484d6fee07c3ec283a661005faa941c258e0
kotahorii <khorii.micron@gmail.com>
2022-01-17 20:08:37 +0900

    [Update] test.cs

---
+++ PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+  [SerializeField] private float moveSpeed = 3;
+  [SerializeField] private float jumpPower = 3;
+  private CharacterController _characterController;
+  private Transform _transform;
+  private Vector3 _moveVelocity;
+  // Start is called before the first frame update
+  void Start()
+  {
+    _characterController = GetComponent<CharacterController>();
+    _transform = transform;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    UnityEngine.Debug.Log(_characterController.isGrounded ? "地上にいます" : "空中です");
+
+    _moveVelocity.x = Input.GetAxis("Horizontal") * moveSpeed;
+    _moveVelocity.z = Input.GetAxis("Vertical") * moveSpeed;
+
+    _transform.LookAt(_transform.position + new UnityEngine.Vector3(_moveVelocity.x, 0, _moveVelocity.z));
+    if (_characterController.isGrounded)
+    {
+      if (Input.GetButtonDown("Jump"))
+      {
+        UnityEngine.Debug.Log("ジャンプ！");
+        _moveVelocity.y = jumpPower;
+      }
+    }
+    else
+    {
+      _moveVelocity.y += Physics.gravity.y * Time.deltaTime;
+    }
+
+    _characterController.Move(_moveVelocity * Time.deltaTime);
+  }
+}
